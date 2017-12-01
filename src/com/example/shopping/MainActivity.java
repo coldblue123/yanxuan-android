1544f0e0a4326108b4d9ee18cd33a1fdd5a95b7f
coldblue <792202228@qq.com>
2017-12-01 23:42:46 +0800

    轮播图

--- src/com/example/shopping/MainActivity.java
+++ src/com/example/shopping/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.shopping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,14 +13,31 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.androiddevelop.cycleviewpager.lib.CycleViewPager;
+import cn.androiddevelop.cycleviewpager.lib.CycleViewPager.ImageCycleViewListener;
 
 import com.example.shopping.Model.Quanju;
+import com.stevenhu.android.phone.bean.ADInfo;
 
 public class MainActivity extends Activity {
 
 	private LinearLayout layout_menu_1, layout_menu_2, layout_menu_3,
 			layout_menu_4;
 	Intent intent;
+	
+	
+	private List<ImageView> views = new ArrayList<ImageView>();
+	private List<ADInfo> infos = new ArrayList<ADInfo>();
+	private CycleViewPager cycleViewPager;
+	
+	private int[] imageUrls = {
+			R.drawable.banner_test,
+			R.drawable.banner_test,
+			R.drawable.banner_test,
+			};
+	
 
 	Quanju q; // 定义全局类
 	private ImageView imagv_1, imagv_2, imagv_3, imagv_4;
@@ -34,6 +55,8 @@ public class MainActivity extends Activity {
 		routerPageFun();
 		 // 搜索事件初始化加载 
 		  btFindGo();
+		// 初始化banner轮播图
+			initialize();
 	}
 
 	@Override
@@ -109,5 +132,64 @@ public class MainActivity extends Activity {
 			});
 
 		}
+		
+		
+		@SuppressLint("NewApi")
+		private void initialize() {
+			
+			cycleViewPager = (CycleViewPager) getFragmentManager()
+					.findFragmentById(R.id.fragment_cycle_viewpager_content);
+			
+			for(int i = 0; i < imageUrls.length; i ++){
+				ADInfo info = new ADInfo();
+				info.setUrl(imageUrls[i]);
+				info.setContent("图片-->" + i );
+				infos.add(info);
+			}
+			
+			
+			ImageView imageView = new ImageView(this);  
+			imageView.setBackgroundResource(imageUrls[0]);
+			views.add(imageView);
+			
+			for (int i = 0; i < infos.size(); i++) {
+				ImageView imageView2 = new ImageView(this);  
+				imageView2.setBackgroundResource(imageUrls[i]);
+				views.add(imageView2);
+			}
+			// 将第一个ImageView添加进来
+			ImageView imageView3 = new ImageView(this);  
+			imageView3.setBackgroundResource(imageUrls[2]);
+			views.add(imageView3);
+			
+			// 设置循环，在调用setData方法前调用
+			cycleViewPager.setCycle(true);
+
+			// 在加载数据前设置是否循环
+			cycleViewPager.setData(views, infos, mAdCycleViewListener);
+			//设置轮播
+			cycleViewPager.setWheel(false);
+
+		    // 设置轮播时间，默认5000ms
+			cycleViewPager.setTime(2000);
+			//设置圆点指示图标组居中显示，默认靠右
+//			cycleViewPager.setIndicatorCenter();
+		}
+		
+		// 点击图片之后的监听操作
+		private ImageCycleViewListener mAdCycleViewListener = new ImageCycleViewListener() {
+
+			@Override
+			public void onImageClick(ADInfo info, int position, View imageView) {
+				if (cycleViewPager.isCycle()) {
+					position = position - 1;
+					Toast.makeText(MainActivity.this,
+							"position-->" + info.getContent(), Toast.LENGTH_SHORT)
+							.show();
+				}
+				
+			}
+
+		};
 
 }
